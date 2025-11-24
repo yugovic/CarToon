43b106c638e7c4f26ec93d58a679b1237de634c5
yugovic <yugovic7@gmail.com>
2025-11-24 22:56:00 +0900

    Favicon修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "MiniCartoon",
   description: "ミニカーをアップロードして、リアルから漫画調へワンタップ変換。",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
